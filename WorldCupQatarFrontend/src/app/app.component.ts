9635c3a2828d55aa908f8b79d44e6a02ab96f371
Igor <igornedovic@gmail.com>
2023-03-19 18:18:33 +0100

    Added frontend initial project

---
+++ WorldCupQatarFrontend/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  constructor(private toastr: ToastrService) {}
+
+  onButtonClick()
+  {
+    this.toastr.success("This is a common toastr message!");
+  }
+}
