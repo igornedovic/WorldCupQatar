1b53f5044338bd7d5b48fd4c2e081eeeb867a3a0
Igor <igornedovic@gmail.com>
2023-04-03 22:55:00 +0200

    Added minor changes to ui components logic

--- WorldCupQatarFrontend/src/app/manage-matches/match-preview/match-preview.component.ts
+++ WorldCupQatarFrontend/src/app/manage-matches/match-preview/match-preview.component.ts
@@ -1,17 +1,44 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { MatchInterface } from 'src/app/models/match.model';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+
+import { MatchInterface, MatchStatus } from 'src/app/models/match.model';
 
 @Component({
   selector: 'app-match-preview',
   templateUrl: './match-preview.component.html',
   styleUrls: ['./match-preview.component.css']
 })
-export class MatchPreviewComponent implements OnInit {
+export class MatchPreviewComponent implements OnInit, AfterViewInit {
   @Input() match!: MatchInterface;
+  public matchStatuses = Object.values(MatchStatus);
+  @ViewChild('selectedStatus') selectedStatusRef!: ElementRef;
+  @ViewChild('changeStatusCheckbox') changeStatusCheckboxRef!: ElementRef;
+  @ViewChild('saveButton') saveButton!: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.selectedStatusRef.nativeElement.disabled = true;
+    this.saveButton.nativeElement.disabled = true;
+  }
+
+  onStatusChange() {
+    if (this.selectedStatusRef.nativeElement.value == MatchStatus.NotStarted)
+    {
+      this.saveButton.nativeElement.disabled = true;
+      return;
+    }
+
+    this.saveButton.nativeElement.disabled = false;
+  }
+
+  onChangeCheckbox(changeStatusCheckbox: HTMLInputElement) {
+    if (changeStatusCheckbox.checked) {
+      this.selectedStatusRef.nativeElement.disabled = false;   
+    } else {
+      this.selectedStatusRef.nativeElement.disabled = true;
+    }
   }
 
 }
